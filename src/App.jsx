a930bf02a124d3f50559bf34da338470784ae166
shivakumar <shivubiradar2k@gmail.com>
2024-12-13 13:17:46 +0530

    fushing project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DailyTaskList from "./To-Do.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <DailyTaskList />
+    </div>
+  );
+}
+
+export default App;
